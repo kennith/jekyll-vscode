5f8ab631aa4c6060fe4df88ea9ab2f4977d611a1
Kennith Leung <kennith.leung@gmail.com>
2024-01-06 13:35:51 -0800

    move template files

--- src/extension.ts
+++ src/extension.ts
@@ -23,7 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
 		if (postTitle && categories) {
 
 			let filename = filePrefix() + '-' + postTitle.replaceAll(' ', '-') + '.markdown';
-			let templatefile = (await vscode.workspace.fs.readFile(vscode.Uri.file(context.extensionPath + '/src/templates/post.template'))).toString();
+			let templatefile = (await vscode.workspace.fs.readFile(vscode.Uri.file(context.extensionPath + '/templates/post.template'))).toString();
 			let formattedTemplatefile = templatefile.replaceAll('{{post-title}}', startCase(postTitle))
 				.replaceAll('{{post-categories}}', categories);
 
